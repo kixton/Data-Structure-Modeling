c5a451869062ca282881db6cb4c7a36c32502e1e
Kim Pham <ms.kimpham@gmail.com>
2014-08-26 13:30:00 -0700

    initial commit

---
+++ recipe.rb
@@ -0,0 +1,42 @@
+
+
+recipes = [
+
+	{
+	recipe_name: "apple salad",
+	serving_size: 4,
+	ingredients: {
+		"mixed greens" => "6 oz",
+		"honey mustard" => "1 tbs",
+		"almonds" => "2 tbs",
+		"applies" => "4",
+		"salt",
+		"pepper"		
+		}
+	}
+
+]
+
+
+apple salad
+serves 4
+6 oz mixed greens
+1 tbs honey mustard
+2 tbs almonds
+4 apples
+salt and pepper
+
+bean burgers
+serves 4
+2 lbs black beans
+4 buns
+lettuce
+8 tomatoes, sliced thinly
+1 cup feta cheese
+mustard
+
+avacado soup
+serves 2
+1 avacado
+1 soup
+cookies
