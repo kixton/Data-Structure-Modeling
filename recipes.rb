10bca87f1844a3641b874ca6c1bb8c8057d06def
Kim Pham <ms.kimpham@gmail.com>
2014-08-26 17:21:07 -0700

    clean up Recipes class; Part I: array of each line; Part II: hash of 3 recipes; Part III: create instances of Recipe

--- recipes.rb
+++ recipes.rb
@@ -1,65 +1,62 @@
-
-
-
 class Recipe
 
 	attr_reader :name, :serving, :ingredients
 
-	def initialize(name, serving_size)
+	def initialize(name, serving, ingredients) # ingredients will be an array
 		@name = name
-		@serving = serving_size
-		@ingredients = []
-	end	
-
-	def add_ingredient(ingredient)
-		@ingredients << ingredient 
+		@serving = serving
+		@ingredients = ingredients 
 	end	
 
 end	
 
-
-array = []
-file = File.open("recipes.txt", "r")
-	file.each_line do |line|
-
-		array << line
-		array.split("/n")
-
+# Part I -- open file and create an array of all lines
+array = [] # create an array
+file = File.open("recipes.txt", "r") # open recipes.txt file
+	file.each_line do |line| # loop through each line in the file
+		array << line # put in each line in an array
 	end
-file.close
+file.close # close file 
 
-puts array
 
-=begin
+# Part II -- Create a hash with 3 pairs for each of the 3 recipes. Key = 0..2, Value = recipe info (name, serving size, ingredients)
+count = 0 # counter
+hash = {} # create blank new hash
+array2 = [] # create blank new array2
 
-file.each_line do |line|
+array.each do |x| # loop through array from Part I
+	if x == "\n" # if element is blank...
+		hash["#{count}"] = array2 # ...assign that array to a hash with the key = counter
+		count += 1 # then increment counter (so you can later assign it to a new hash key)
+		array2 = [] # empty array2
+	else 
+		x.gsub!("\n","") # take the element and subsitute "\n" with blank string to remove the "\n" at the end of the string
+ 		array2 << x # push element into array2
+	end	
+end	
 
-	# first create an array for each recipe
-		apple_salad = []
-		bean_burgers = []
-		avacado_soup = []
-	
-	# store each line as string
-		apple_salad << line
-		bean_burgers << line
-		avacado_soup << line
+# {"0" => ["recipe name", "serves 2", "ingredient 1", "ingredient 2"], "1" => [etc], "2" => [etc]}
 
-	# read array 
-		array[0] # index 0 --> make it a instance of a Recipe  
-		array[1] # index 2 --> serving size 
-		array[3..-1] # index 3 to last index = ingredients
 
+# Part III -- create instances of Recipe class
+array3 = [] # create blank new array3
+
+hash.each do |k, v| # loop through the hash (created in Part II)
+	array3 << Recipe.new(v[0], v[1], v[2..-1]) 
+		# create new instance of Recipe which takes 3 arguments: name, serving, ingredients
+		# values are stored as an array
+		# index 0 of the array is the recipe name (first argument)
+		# pushes the new instance of Recipe into array3
 end
 
 
-f.close	
 
 
-
-apple_salad = Recipe.new("apple salad", "4")
-apple_salad.add_ingredient("6 oz mixed greens")
-
-
+=begin
+	
+rescue Exception => e
+	
+end
 # Desired Solution
 
 recipes = {
